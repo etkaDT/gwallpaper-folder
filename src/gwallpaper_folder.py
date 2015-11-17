801010305d28d0b67ca4bec40c852c88accfdca7
Denis Thulin <denis.thulin@gmail.com>
2015-11-17 14:31:13 +0100

    added locksreen settings

--- src/gwallpaper_folder.py
+++ src/gwallpaper_folder.py
@@ -4,10 +4,17 @@ import random
 from gi.repository import Gio
 
 class wallpaperChanger(object):
-    def __init__(self, folder=None):
+    settings = {"background": "org.gnome.desktop.background",
+                "locksreen" : "org.gnome.desktop.screensaver"}
+    def __init__(self, folder=None, user=None, setting="background"):
         self.folder = folder
         self.chosen_one = None
-        self.setting = Gio.Settings.new("org.gnome.desktop.background")
+        self.setting = Gio.Settings.new(self.settings[setting])
+        self.user = user
+        if user:
+            self.change = self.su_change
+        else:
+            self.change = self.std_change
 
     def select_folder(self, folder):
         self.folder = folder
@@ -20,16 +27,24 @@ class wallpaperChanger(object):
         files = os.listdir(self.folder)
         self.chosen_one = random.choice(files)
 
-    def change(self):
+    def std_change(self, user=None):
         if not self.chosen_one or not self.folder:
             return
         full_path = os.path.join(self.folder, self.chosen_one)
         wallpaper = "file://{path}".format(path=full_path)
         self.setting.set_string("picture-uri", wallpaper)
 
+    def su_change(self, user):
+        pass
+
 
 if __name__ == "__main__":
-    wpc = wallpaperChanger()
-    wpc.select_default_folder()
-    wpc.pick()
-    wpc.change()
+    wpc_w = wallpaperChanger(setting="background")
+    wpc_w.select_default_folder()
+    wpc_w.pick()
+    wpc_w.change()
+
+    wpc_w = wallpaperChanger(setting="locksreen")
+    wpc_w.select_default_folder()
+    wpc_w.pick()
+    wpc_w.change()
